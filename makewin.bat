047dab076235bdb0785c54b2ae2a0db8b3516394
Angel Xex <darkxex@gmail.com>
2021-02-27 12:01:08 -0300

    Make
    Edit Make

--- makewin.bat
+++ makewin.bat
@@ -1,5 +1,2 @@
-@RD /S /Q "C:\respaldo2017\RipJKAnimeNX\build"
-xcopy "C:\respaldo2017\C++\test\test\*.cpp" "C:\respaldo2017\RipJKAnimeNX\source\*.cpp" /y
 make
 nxlink -s "out/RipJKAnime_NX.nro" 
-pause
