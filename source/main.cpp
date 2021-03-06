bc3d406a9101298b4ca77d8698e8c80420d4a203
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-03-06 20:30:27 +0000

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1115,7 +1115,7 @@ int main(int argc, char **argv)
 					gTextTexture.loadFromRenderedText(GOD.gFont, "Usa el HomeBrew PPlay para reproducir el video.", textColor);
 					gTextTexture.render(posxbase, posybase + 260);
 
-					if (std::to_string(porcendown) == "100") {
+					if (std::to_string(porcendown) == "100"&&!isDownloading) {
 						//Render red filled quad
 						VOX.render_VOX({ posxbase + 98, posybase + 400, 580, 50 }, 255, 255, 255, 255);
 						gTextTexture.loadFromRenderedText(GOD.gFont3, "¡Descarga Completada! Revisa tu SD.", textColor);
