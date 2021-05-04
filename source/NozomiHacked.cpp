720dc47b54be290ee32cc1ef4c7e8f9f927222cc
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-05-04 14:19:39 +0100

    short the code a bit

--- source/NozomiHacked.cpp
+++ source/NozomiHacked.cpp
@@ -20,13 +20,12 @@ static size_t SecondCallback(void *contents, size_t size, size_t nmemb, void *us
 	((std::string*)userp)->append((char*)contents, size * nmemb);
 	return size * nmemb;
 }
+
 std::string getFirstKey(std::string enlace)
 {
-
 	CURL *curl;
 	CURLcode res = CURLE_OK;
 	std::string Buffer;
-
 	curl = curl_easy_init();
 	if (curl) {
 		curl_easy_setopt(curl, CURLOPT_URL, enlace.c_str());
@@ -42,14 +41,10 @@ std::string getFirstKey(std::string enlace)
 		if (res != CURLE_OK){printf("\n%s\n",curl_easy_strerror(res));}
 	}
 
-     std::string codetemp = Buffer;
-	   int temp1, temp2;
-	   temp1 = codetemp.find("name=\"data\" value=\"");
-	   temp1 = temp1 +19;
-	   temp2 = codetemp.find("\"",temp1);
-	  codetemp = codetemp.substr(temp1,temp2 - temp1);
-	   std::cout << "FirstKey: "<< codetemp << std::endl;
-       Buffer = codetemp;
+	std::string codetemp = scrapElement(Buffer,"name=\"data\" value=\"", "\"");
+	replace(codetemp,"name=\"data\" value=\"","");
+	std::cout << "FirstKey: "<< codetemp << std::endl;
+    Buffer = codetemp;
 	return Buffer;
 }
 std::string getSecondKey(std::string data)
@@ -58,45 +53,35 @@ std::string getSecondKey(std::string data)
    std::string redirected = "";
 	CURL *curl;
     CURLcode curl_res;
-
     curl_global_init(CURL_GLOBAL_ALL);
-
     curl = curl_easy_init();
-
     if (curl)
     {
-		
         curl_easy_setopt(curl, CURLOPT_URL, "https://jkanime.net/gsplay/redirect_post.php");
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
-		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-		
-        	curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
-		 curl_easy_setopt(curl, CURLOPT_REFERER, "https://jkanime.net/");
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+		curl_easy_setopt(curl, CURLOPT_REFERER, "https://jkanime.net/");
 
         /* Perform the request, curl_res will get the return code */ 
         curl_res = curl_easy_perform(curl);
 
         /* Check for errors */ 
         if(curl_res != CURLE_OK)
-          fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                  curl_easy_strerror(curl_res));
+          fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(curl_res));
 
         if(CURLE_OK == curl_res) 
         {
-             char *url;
-             curl_res = curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
-
-             if((CURLE_OK == curl_res) && url)
-                { printf("CURLINFO_EFFECTIVE_URL: %s\n", url);
-				redirected = url;}
+			char *url;
+			curl_res = curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
+			if((CURLE_OK == curl_res) && url)
+             {
+				printf("CURLINFO_EFFECTIVE_URL: %s\n", url);
+				redirected = url;
+			}
         }
-
-        /* always cleanup */ 
         curl_easy_cleanup(curl);
-
-        /* we're done with libcurl, so clean it up */ 
         curl_global_cleanup();
-
     }
     else
     {
@@ -104,15 +89,14 @@ std::string getSecondKey(std::string data)
     }
      std::cout << "Secondkey: "<< redirected << std::endl;
 return redirected;
-
 }
 
 std::string getThirdKey(std::string secondKey)
 {
 
+	std::string Buffer;
 	CURL *curl;
 	CURLcode res = CURLE_OK;
-	std::string Buffer;
     std::string second = "v=" + secondKey;
     replace(second,"https://jkanime.net/gsplay/player.html#","");
 	curl = curl_easy_init();
@@ -130,15 +114,10 @@ std::string getThirdKey(std::string secondKey)
 		curl_easy_cleanup(curl);
 		if (res != CURLE_OK){printf("\n%s\n",curl_easy_strerror(res));}
 	}
-
-     std::string codetemp = Buffer;
-	   int temp1, temp2;
-	   temp1 = codetemp.find("https:");
-	   temp2 = codetemp.find("\"",temp1);
-	  codetemp = codetemp.substr(temp1,temp2 - temp1);
-      replace(codetemp,"\\","");
-	   std::cout << "ThirdKey: "<< codetemp << std::endl;
-       Buffer = codetemp;
+	std::string codetemp = scrapElement(Buffer,"https:", "\"");
+	replace(codetemp,"\\","");
+	std::cout << "ThirdKey: "<< codetemp << std::endl;
+	Buffer = codetemp;
 	return Buffer;
 }
 
