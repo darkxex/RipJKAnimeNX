cddf16eb9b339f3068951d3ef9736fcd2714e43b
Angel Xex <darkxex@gmail.com>
2021-05-04 20:30:05 -0400

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1011,7 +1011,7 @@ int main(int argc, char **argv)
 					#else
 						std::string TYPEA =  "sdmc";
 					#endif
-					gTextTexture.loadFromRenderedText(GOD.gFont, (TYPEA+" (Ver "+VERCAT+") #PIï¿½ERAXUPALO").c_str(), {100,0,0});
+					gTextTexture.loadFromRenderedText(GOD.gFont, (TYPEA+" (Ver "+VERCAT+") #PIÑERAXUPALO").c_str(), {100,0,0});
 					gTextTexture.render(SCREEN_WIDTH - gTextTexture.getWidth() - 30, 20);
 					if (imgNumbuffer > 0){
 						gTextTexture.loadFromRenderedText(GOD.gFont, "Imagenes: ("+std::to_string(imgNumbuffer)+"/30)", {0,100,0});
