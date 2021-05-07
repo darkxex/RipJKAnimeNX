1dabd3f4a20c03b897b86754f65895bcbc8fe339
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-05-07 18:10:01 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -3,7 +3,6 @@ rem comenta esto para desactivar el uso de la nand como root
 set USENAND=true
 set EXPORTNSP=true
 title RipJKAnime_NX
-del out\RipJKAnime*.nsp /q>nul
 make -j20
 set a=%errorlevel%
 echo ------------------------------------------
@@ -18,4 +17,5 @@ echo -----------------------------------
 
 %systemroot%\system32\timeout.exe 20
 %systemroot%\system32\timeout.exe 20
+
 
