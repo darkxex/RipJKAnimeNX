710d1afe5dfed6764908b54828c8f93edf6f2e2a
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-07-27 10:51:32 +0100

    fit fembeed

--- source/utils.hpp
+++ source/utils.hpp
@@ -1,10 +1,10 @@
-
+#include <vector>
 bool isFileExist(std::string file);
 void replace(std::string& subject, const std::string& search,const std::string& replace);
 void mayus(std::string &s);
 bool onlinejkanimevideo(std::string onlineenlace,std::string server);
 bool linktodownoadjkanime(std::string urltodownload,std::string directorydownload);
 std::string scrapElement(std::string content, std::string get,std::string delim ="" );
-std::string scrapElementReverse(std::string content, std::string get,std::string delim ="" );
+std::vector<std::string> scrapElementAll(std::string content, std::string get,std::string delim ="" );
 void touch(std::string route);
 bool copy_me(std::string origen, std::string destino);
