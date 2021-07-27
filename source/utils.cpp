9fc42ed41d8b7d94d13b6a685132da95ac70ca82
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2021-07-27 01:39:06 +0100

    ups

--- source/utils.cpp
+++ source/utils.cpp
@@ -85,6 +85,22 @@ std::string scrapElementReverse(std::string content, std::string get, std::strin
 	return Element;
 }
 
+using namespace std;
+// for string delimiter
+vector<string> split (string s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
 
 //Mixdrop link decode
 std::string MD_s(std::string code){
